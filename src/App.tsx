e490459fa351c242c156d1017a543c715df862f5
Annie <annieteng8022@gmail.com>
2024-01-11 13:38:34 +0800

    update basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
+import AGILE from './image/agile.jpg'
 import './App.css';
+import {motion} from 'framer-motion'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <img src={AGILE} alt="image" width="40%"/>
+        <motion.p
+          initial={{ x: 0, opacity: 0 }}
+          animate={{ x: 30, opacity: 1 }}
+          exit={{ x: 0, opacity: 0 }}>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        </motion.p>
         <a
           className="App-link"
           href="https://reactjs.org"
